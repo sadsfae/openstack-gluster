ce2691adc6269b7b82214d3fb10fbccab647e971
Marek Mahut <mmahut@redhat.com>
2013-09-23 12:52:05 +0200

    properly parse the variables from the host yaml file

--- gluster/manifests/server/cinder.pp
+++ gluster/manifests/server/cinder.pp
@@ -12,6 +12,21 @@ class gluster::server::cinder {
     # exec glusterfs command to create cinder vol if it doesn't exist, join cluster
     if $cinder_exists == "0" {
 
+    $replicacount = $::replicacount ? {
+        ''      => 2,
+         default => $::replicacount
+    }
+
+    $mystorageip = $::mystorageip ? {
+        ''      => false,
+        default => $::mystorageip
+    }
+
+    $gluster_server_peers = $::gluster_server_peers
+        ''      => false,
+        default => $::gluster_server_peers
+    }
+
     exec { "gluster create volume cinder":
       command => "gluster volume create cinder_vol replica ${replicacount} ${mystorageip}:/srv/gluster/glance ${gluster_server_peers}:/srv/gluster/glance",
     }
