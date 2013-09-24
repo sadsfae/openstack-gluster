9da04f7762f931f933479d9d4cf3a62ead19dbd7
Gilles Dubreuil <gdubreui@redhat.com>
2013-09-24 16:58:00 +1000

    adding (missing) volume provider

---
+++ lib/puppet/provider/volume/gluster.rb
@@ -0,0 +1,60 @@
+require 'puppet'
+
+Puppet::Type.type(:volume).provide(:gluster) do
+  desc 'glusterfs volume management'
+
+  optional_commands :gluster => '/usr/sbin/gluster'
+
+  def exists?
+    gluster('volume', 'status', @resource[:name]) 
+  rescue Puppet::ExecutionFailure
+    return false
+  end
+
+  def create
+    if @resource[:bricks].class == String
+      gluster('volume', 'create', @resource[:name], bricks_list)
+    else
+      probe
+      gluster('volume', 'create', @resource[:name],
+        "replica #{@resource[:bricks].size}", bricks_list)
+    end
+
+    gluster('volume', 'start', @resource[:name])
+  end
+
+  def destroy
+    # Not working: gluster command needs confirmation!
+    # gluster('volume', 'stop', @resource[:name])
+    # gluster('volume', 'delete', @resource[:name])
+  end
+
+  private 
+
+  def bricks_list
+    volume = File.join(@resource[:path], @resource[:name])
+    list = ''
+    @resource[:bricks].each do |brick|
+      list << "#{brick}:#{volume}"
+    end
+    return list
+  end
+
+  def info?(str)
+    value = nil
+    res = %x(/usr/sbin/gluster volume info #{@resource[:name]})
+    if $?.success?
+      res.each_line do |line|
+        value = true if line =~ /#{str}/i
+      end
+    end
+    return value
+  end
+
+  def probe
+    @resource[:bricks].each do |peer|
+      gluster('peer', 'probe', peer)
+    end
+  end
+
+end
