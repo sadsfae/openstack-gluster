d6098a7e1ec41c0c2ed1e6e270def0f586d85b42
Gilles Dubreuil <gdubreui@redhat.com>
2013-09-24 17:50:44 +1000

    Fixed geo_replication variable

--- manifests/brick.pp
+++ manifests/brick.pp
@@ -1,15 +1,15 @@
 # bare minimum needed for a new glusterfs brick
 class gluster::brick (
   $server_package_name          = $gluster::params::server_package_name,
-  $geo-replication_package_name = $gluster::params::geo-replication_package_name,
+  $geo_replication_package_name = $gluster::params::geo_replication_package_name,
   $server_service_name          = $gluster::params::server_service_name,
   ) inherits gluster::params {
     Package {
         ensure => installed
     }
 
-  package { $server_package_name : }
-  package { $geo-replication_package_name : }
+    package { $server_package_name : }
+    package { $geo_replication_package_name : }
 
 # Included as dependencies:
 #    package { 'glusterfs' : }
--- manifests/params.pp
+++ manifests/params.pp
@@ -2,10 +2,10 @@ class gluster::params {
   case $::osfamily {
     'RedHat': {
       # Package
-      $server_package_name              = 'glusterfs-server' 
-      $$geo-replication_package_name    = 'glusterfs-geo-replication'
+      $server_package_name          = 'glusterfs-server' 
+      $geo_replication_package_name = 'glusterfs-geo-replication'
 	 	  # Services
-			$server_service_name              = 'glusterd'
+			$server_service_name          = 'glusterd'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat")
