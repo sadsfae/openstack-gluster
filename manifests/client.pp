b32c1fadf7e18c68810b4a5c548f22502132b707
Will Foster <wfoster@redhat.com>
2013-09-25 15:28:44 +0200

    Foreman does not like package wrangling syntax style like what is used in brick.pp even though it passes lint :(

--- manifests/client.pp
+++ manifests/client.pp
@@ -1,13 +1,7 @@
 # setup the minimum for an OpenStack GlusterFS client
-class gluster::client (
-  $client_package_name          = $gluster::params::client_package_name,
-  ) inherits gluster::params {
-
-  Package {
-    ensure => installed
-  }
-
-  package { [ $client_package_name, $geo_replication_package_name ]:
-  } ->
-
+class gluster::client {
+    
+    package { 'glusterfs-fuse':
+        ensure => installed,
+    }
 }
