25689b7769b23ce61b0f1d9fd9fdd5d28fa1e7a4
Will Foster <wfoster@redhat.com>
2013-09-25 13:51:20 +0200

    create client.pp for bare-minimum usage of glusterfs mounts, update params.pp that are referenced

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,11 +1,13 @@
+# Define commonly used parameters for server and client
 class gluster::params {
   case $::osfamily {
     'RedHat': {
-      # Package
-      $server_package_name          = 'glusterfs-server' 
+      # Packages
+      $server_package_name          = 'glusterfs-server'
       $geo_replication_package_name = 'glusterfs-geo-replication'
-	 	  # Services
-			$server_service_name          = 'glusterd'
+      $client_package_name          = 'glusterfs-fuse'
+      # Services
+      $server_service_name          = 'glusterd'
     }
     default: {
       fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat")
